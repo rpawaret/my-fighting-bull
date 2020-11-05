830400879e654a50f1704541077656da4ab7d71f
pawaret <pawaret.r@gmail.com>
2020-11-05 15:54:10 +0700

    add ngx-auth firebaseui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-fighting-bull';
+
+  printUser(event) {
+    console.log(event);
+  }
+
+  printError(event) {
+    console.error(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,37 @@ import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BullModule } from './bull/bull.module';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     BullModule,
+    NgxAuthFirebaseUIModule.forRoot(
+      environment.firebaseConfig,
+      () => 'my-fighting-bull',
+      {
+        enableFirestoreSync: true, // enable/disable autosync users with firestore
+        toastMessageOnAuthSuccess: false, // whether to open/show a snackbar message on auth success - default : true
+        toastMessageOnAuthError: false, // whether to open/show a snackbar message on auth error - default : true
+        authGuardFallbackURL: '/loggedout', // url for unauthenticated users - to use in combination with canActivate feature on a route
+        authGuardLoggedInURL: '/loggedin', // url for authenticated users - to use in combination with canActivate feature on a route
+        passwordMaxLength: 60, // `min/max` input parameters in components should be within this range.
+        passwordMinLength: 4, // Password length min/max in forms independently of each componenet min/max.
+        // Same as password but for the name
+        nameMaxLength: 50,
+        nameMinLength: 2,
+        // If set, sign-in/up form is not available until email has been verified.
+        // Plus protected routes are still protected even though user is connected.
+        guardProtectedRoutesUntilEmailIsVerified: true,
+        enableEmailVerification: true, // default: true
+      }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent],
