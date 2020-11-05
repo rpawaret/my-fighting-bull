b1e2d38fab2bcb211efa947b5a6db8fc780b80d3
pawaret <pawaret.r@gmail.com>
2020-11-05 11:47:08 +0700

    add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBLdWSeh_YsBE-0OZBULgZU86bjGO84I88',
+    authDomain: 'my-fighting-bull.firebaseapp.com',
+    databaseURL: 'https://my-fighting-bull.firebaseio.com',
+    projectId: 'my-fighting-bull',
+    storageBucket: 'my-fighting-bull.appspot.com',
+    messagingSenderId: '146503617612',
+    appId: '1:146503617612:web:82092765ed23970e3778e9',
+    measurementId: 'G-M0ML8JE105',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBLdWSeh_YsBE-0OZBULgZU86bjGO84I88',
+    authDomain: 'my-fighting-bull.firebaseapp.com',
+    databaseURL: 'https://my-fighting-bull.firebaseio.com',
+    projectId: 'my-fighting-bull',
+    storageBucket: 'my-fighting-bull.appspot.com',
+    messagingSenderId: '146503617612',
+    appId: '1:146503617612:web:82092765ed23970e3778e9',
+    measurementId: 'G-M0ML8JE105',
+  },
 };
 
 /*
